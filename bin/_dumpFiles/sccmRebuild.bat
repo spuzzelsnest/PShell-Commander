a87ca780e168f576fc557528212b1cc079592d27
unknown <jan.mpdesmet@gmail.com>
2017-02-21 22:29:55 +0100

    sync with test at work

--- bin/_dumpFiles/sccmRebuild.bat
+++ bin/_dumpFiles/sccmRebuild.bat
@@ -1,16 +1,51 @@
 ::sccmRebuild.bat
+
 ::Rebuild sccm 
+
 ::
+
 ::Created by Spuzzelsnest
+
 ::
+
 ::Change Log
+
 ::-----------
+
 ::V1.0 Initial release 30-08-2016
+
 ::############################################
+
 @echo off
+
 set %Wdir%="C:\windows\System32\wbem"
+
+sc config winmgmt start= disabled
 net pause Winmgmt
-move %wdir%\Repository %wdir%\repository.old
+
+
+
+Winmgmt /salvagerepository %wdir%
+
+
+winmgmt /resetrepository %wdir%
+
+for %i in (*.dll) do RegSvr32 -s %i
+
+for %i in (*.exe) do %i /RegServer
+
+
+
+cd /d %windir%\sysWOW64\wbem
+
+for %i in (*.dll) do RegSvr32 -s %i
+
+for %i in (*.exe) do %i /RegServer
+
 net continue winmgmt
-C:\windows\ccmsetup\ccmsetup.exe
+
+
+cd C:\windows\ccmsetup\
+
+ccmsetup.exe
 
