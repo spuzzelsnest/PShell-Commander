c2f2f29cff47702f1f139d2112cd09fd9316db97
spuzzelsnest <jan.mpdesmet@gmail.com>
2017-05-10 16:06:34 +0200

    changed lync to auto find user - ResteIE actually Resets now

--- bin/_dumpFiles/lync.bat
+++ bin/_dumpFiles/lync.bat
@@ -6,15 +6,35 @@
 ::
 ::Change Log
 ::-----------
-::V1.0 Initial release 30-06-2016
+::V1.0 - 30-06-2016 - Initial release
+::V1.1 - 10-05-2017 auto user check from local pc
 ::############################################
 @echo off
-gpupdate /force
-taskkill /f /im lync*
-taskkill /f /im communicator.exe
-taskkill /f /im Outlook.exe
-rd /s /q %userprofile%\appdata\local\microsoft\communicator lync.old
-rd /s /q %userprofile%\appdata\local\microsoft\office\15.0\lync 
-Reg delete HKEY_CURRENT_USER\Software\Microsoft\Communicator /f
-reg delete HKEY_CURRENT_USER\Software\Microsoft\Office\15.0\lync /f
-exit
+cd C:\temp
+
+set loggedon="wmic /node:127.0.0.1 computerSystem Get UserName | findstr %userdomain%"
+
+for /f "tokens=*" %%i IN (' %loggedon% ') do set X=%%i
+for /f "tokens=* delims=%userdomain%\" %%a in ("%X%") do set user=%%a
+
+echo Closing open instances of Communicator/Lync/Skype and Outlook
+
+tasklist /fi "imagename eq lync*" | find ":" > nul
+if errorlevel 1 taskkill /f /im "lync*" 
+
+
+taskkill /fi "imagename eq communicator.exe" | find ":" > nul
+if errorlevel 1 taskkill /f /im "communicator.exe"
+
+taskkill /fi "imagename eq Outlook.exe" | find ":" > nul
+if errorlevel 1 taskkill /f /im "Outlook.exe"
+
+echo Removing local directories for %user%
+
+rd /s /q C:\Users\%user%\appdata\local\microsoft\communicator lync.old
+rd /s /q C:\Users\%user%\appdata\local\microsoft\office\15.0\lync 
+
+::reg delete HKEY_CURRENT_USER\Software\Microsoft\Communicator /f
+::reg delete HKEY_CURRENT_USER\Software\Microsoft\Office\15.0\lync /f
+
+exit /b
--- bin/_dumpFiles/resetIE.bat
+++ bin/_dumpFiles/resetIE.bat
@@ -1,23 +1,13 @@
 ::resetIE.bat
-
 ::Reset Internet settings
-
 ::
-
 ::Created by Spuzzelsnest
-
 ::
-
 ::Change Log
-
 ::-----------
-
 ::V1.0 Initial release 29-08-2016
-
 ::############################################
 
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 4351
 
-
-RunDll32.exe InetCpl.cpl,ResetIEtoDefaults
-
-exit
+exit /b
