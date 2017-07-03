df18b35b0eb517efde372d9b2edbbf504468ce3c
spuzzelsnest <jan.mpdesmet@gmail.com>
2017-07-03 17:31:03 +0200

    wmi.bat update

--- bin/_dumpFiles/WMI.bat
+++ bin/_dumpFiles/WMI.bat
@@ -5,29 +5,28 @@
 ::
 ::Change Log
 ::-----------
-
 ::V1.0 Initial release 30-08-2016
 ::############################################
 @echo off
+cd /d %windir%\system32\wbem
+Winmgmt /salvagerepository
+net pause winmgmt /y
 
-set %Wdir%="C:\windows\System32\wbem"
-
-net pause Winmgmt
-Winmgmt /salvagerepository %wdir%
-
-for %i in (*.dll) do RegSvr32 -s %i
-for %i in (*.exe) do %i /RegServer
+for %i in (*.dll) do RegSvr32 -s %i >> C:\temp\Logs\result.log
+for %i in (*.exe) do %i /RegServer >> C:\temp\Logs\result.log
+ 
 
 cd /d %windir%\sysWOW64\wbem
+for %i in (*.dll) do RegSvr32 -s %i >> C:\temp\Logs\result.log
+for %i in (*.exe) do %i /RegServer >> C:\temp\Logs\result.log
 
-for %i in (*.dll) do RegSvr32 -s %i
-for %i in (*.exe) do %i /RegServer
+net start winmgmt
+for /f %%s in ('dir /s /b *.mof *.mfl') do mofcomp %%s >> C:\temp\Logs\result.log
 
-net continue winmgmt
-
-gpupdate /force
+gpupdate /force >> C:\temp\Logs\result.log
 
 cd C:\windows\ccmsetup\
 ccmsetup.exe
+
 
 exit /b
