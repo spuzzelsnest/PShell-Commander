c8563846f3cbc14141e772e68493bda639db256d
spuzzelsnest <jan.mpdesmet@gmail.com>
2017-07-11 14:02:36 +0200

    added Alive file

--- bin/Alive.ps1
+++ bin/Alive.ps1
@@ -1,50 +1,36 @@
-﻿$PCs = Get-Content $env:USERPROFILE\Desktop\PC-list.txt
+$pcs = Get-Content $env:USERPROFILE\Desktop\PC-list.txt
 $Complete = @{}
 
 Do {
-
-  $PCs | %{
-
-    If (!$Complete.containsValue($_)) {
-
-	  $status = (Test-Connection -ComputerName $_ -Buffersize 16 -count 1 -quiet) 
-	  #-and (Get-WmiObject Win32_ComputerSystem $_ -ErrorAction SilentlyContinue)
-	  
-      
-	   If ($status -eq "True"){
-             $Complete.Add($_)
+  $pcs | %{
+    $status = (!(Test-Connection -CN $_ -BufferSize 16 -Count 1 -ea 0 -quiet))
+    If (!$Complete.ContainsValue($_)){
+       If ($status -eq  $True){
+       $Complete.Add($_,$status)
       }
     }
   }
 
-  #
-  # Build the HTML output
-  #
+# Build the HTML output
+  $Head = "
+    <title>Status Report</title>
+    <meta http-equiv='refresh' content='30' />"
 
-  $Head = "<Center><title>Status Report</title> <meta http-equiv='refresh' content='30' />"
   $Body = @()
-  $Body += "<table><tr><th>PC</th><th>State</th></tr>"
-  $Body += $PCs | %{
-    If ($Complete.containsValue($_)) {
+  $Body += "<center><table><tr><th>Pc Name</th><th>State</th></tr>"
+  $Body += $pcs | %{
+    If ($Complete.Contains($_)) {
     "<tr><td>$_</td><td><font color='green'>Running</font></td></tr>"
     } Else {
     "<tr><td>$_</td><td><font color='red'>Not Available</font></td></tr>"
     }
   }
-  $Body += "</table></Center>"
-
+  $Body += "</table></center>"
   $Html = ConvertTo-Html -Body $Body -Head $Head
 
-  #
-  # Write HTML to the file
-  #
-
-  $Html > "statusPage.html"
-
-  #
-  # Sleep a while
-  #
-
+# save HTML
+  $Html >   "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\pc-report.html"
+  
+# Sleep a while
   Start-Sleep -Seconds 30
-
-} While ($Complete.Count -lt $PCs.Count)
+} While ($Complete.Count -lt $pcs.Count)
