d9fd64319e3fce448f965819eafa2a95106fbdca
Spuzzelsnest <j.mpdesmet@gmail.com>
2020-05-26 21:13:14 +0200

    Alive service rework

--- bin/Alive.ps1
+++ bin/Alive.ps1
@@ -1,36 +1,39 @@
 $Complete = @{}
-
+$dump = $env:USERPROFILE+"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\pc-report.html"
 Do {
   $pcs = Get-Content $env:USERPROFILE\Desktop\PC-list.txt
 
   $pcs | %{
-    $status = (Test-Connection -CN $_ -BufferSize 16 -Count 1 -ea 0 -quiet)
+    $status = (Test-Connection -CN $_ -Count 1 -quiet)
+    $cnName = ([system.net.dns]::GetHostAddresses($_)).hostname
+    Write-Output $cnName
     If (!$Complete.Containskey($_)){
        If ($status -eq  $True){
+    
        $Complete.Add($_,$status)
       }
     }
   }
-
+  
 # Build the HTML output
-  $Head = "
+  $head = "
     <title>Status Report</title>
     <meta http-equiv='refresh' content='30' />"
 
-  $Body = @()
-  $Body += "<center><table><tr><th>Pc Name</th><th>State</th></tr>"
-  $Body += $pcs | %{
+  $body = @()
+  $body += "<center><table><tr><th>Pc Name</th><th>State</th></tr>"
+  $body += $pcs | %{
     If ($Complete.Contains($_)) {
-    "<tr><td>$_</td><td><font color='green'>Running</font></td></tr>"
+    "<tr><td>$_ $cnName</td><td><font color='green'>Running</font></td></tr>"
     } Else {
     "<tr><td>$_</td><td><font color='red'>Not Available</font></td></tr>"
     }
   }
-  $Body += "</table></center>"
-  $Html = ConvertTo-Html -Body $Body -Head $Head
+  $body += "</table></center>"
+  $html = ConvertTo-Html -Body $body -Head $head
 
 # save HTML
-  $Html >   "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\pc-report.html"
+  $html >  $dump
 
 # Sleep a while
   Start-Sleep -Seconds 30
