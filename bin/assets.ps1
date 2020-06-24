97ce0342db41b9993bef97ee2fca17debae1c856
Spuzzelsnest <j.mpdesmet@gmail.com>
2020-06-24 11:31:18 +0200

    added scripts for teams and assets

---
+++ bin/assets.ps1
@@ -0,0 +1,18 @@
+﻿#--------------------------------------------------------------------------------
+#
+# NAME:		Assets.ps1
+#
+# AUTHOR:	Spuzzelsnest
+# EMAIL:	j.mpdesmet@gmail.com
+#
+# COMMENT:
+#           get Assets active on the network
+#
+#
+#       VERSION HISTORY:
+#       1.0     06.18.2020 	- Initial release
+#--------------------------------------------------------------------------------
+# get All the ip's on the range
+
+
+$range = 
