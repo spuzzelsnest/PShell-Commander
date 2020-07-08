2ded77e6fb7fbadddf63f753ca0563c8e84e26bb
Spuzzelsnest <j.mpdesmet@gmail.com>
2020-07-08 09:27:28 +0200

    setup Asset info collection

--- bin/assets.ps1
+++ bin/assets.ps1
@@ -12,7 +12,26 @@
 #       VERSION HISTORY:
 #       1.0     06.18.2020 	- Initial release
 #--------------------------------------------------------------------------------
-# get All the ip's on the range
+# get PC names from the list ans check for default values name, version, 
 
 
-$range = 
+$file = get-content "$env:USERPROFILE\Desktop\PC-list.txt"
+
+foreach ($f in $file){
+
+
+if(!($(New-Object System.Net.NetworkInformation.Ping).SendPingAsync($f).result.status -eq 'Succes')){
+
+     write-host $f "is niet online!"
+
+}else{
+
+    $pcinfo = get-adcomputer $f -Properties * | Select-object samAccountName, OperatingSystem, Lastlogondate
+    
+    $wmi = Get-WmiObject -Computer $f -Class Win32_ComputerSystem
+    
+    $pcinfo
+    $wmi.model
+
+    }
+}
