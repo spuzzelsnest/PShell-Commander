fbc43611f042543b2ac03d066a3b9988b8e94954
Spuzzelsnest <j.mpdesmet@gmail.com>
2020-09-07 09:11:22 +0200

    mass run

---
+++ massRun.ps1
@@ -0,0 +1,7 @@
+﻿$parameters = @{
+  ComputerName = (Get-Content -Path  c:\TEST-list.txt)
+  InDisconnectedSession = $true
+  FilePath = "C:\PShell-Commander\tryChoco.ps1"
+  SessionOption = @{OutputBufferingMode="Drop";IdleTimeout=632000000}
+}
+Invoke-Command @parameters
