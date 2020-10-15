4f6c357a7e10153ca941e0d5efe15f43137a00b5
Spuzzelsnest <j.mpdesmet@gmail.com>
2020-10-15 21:35:07 +0200

    set percentage right for progress bar

--- bin/Alive.ps1
+++ bin/Alive.ps1
@@ -1,27 +1,48 @@
-$Complete = @{}
-$dump = "bin\Logs\pc-report.html"
-$i = 0
-Do {
+#--------------------------------------------------------------------------------
+#
+# NAME:		pShell-Commander.ps1
+#
+# AUTHOR:	Spuzzelsnest
+#
+# COMMENT:
+#			Network scan - Automating the job
+#
+#
+#       VERSION HISTORY:
+#       1.7     07.11.2017  - Alive Service
+#       2.3.1   26.05.2020  - Rework of the Alive Service
+#       2.3.2   15-10-2020  - Change dir for Alive to Logs 
+#                           - Removed auto start for webpage
+#       2.4.0   15-10-2020  - Stanalone App - no service anymore
+#--------------------------------------------------------------------------------
+# START VARS
 
-  $pcs = Get-Content $env:USERPROFILE\Desktop\PC-list.txt
+$pcs = Get-Content bin\Logs\PC-list.txt
+$dump = "bin\Logs\"
+$file = "network-report.html"
+$tot = ($pcs | Measure-Object -Line).lines
+$i = 1
+ 
+#make backup 
+
+if (Test-Path $dump\$file){ 
+
+copy-item $dump\$file -destination $dump\$file-$(Get-Date -format "yyyy_MM_dd_hh_mm_ss")
+}
+
+
 
   $pcs | %{
 
+     $j = [math]::Round((($i / $tot) * 100),2)
 
+    Write-Progress -Activity "Creating report" -Status "$j% Complete:" -PercentComplete $j
    
-    Write-Progress -Activity "Search in Progress" -Status "$i% Complete:" -PercentComplete $i;
-    
 
     $status = (Test-Connection -CN $_ -Count 1 -quiet)
 
     Write-Output $cnName
-    If (!$Complete.Containskey($_)){
-       If ($status -eq  $True){
-    
-       $Complete.Add($_,$status)
-
-      }
-    }
+   
     $i++
   }
   
@@ -43,8 +64,4 @@ Do {
   $html = ConvertTo-Html -Body $body -Head $head
 
 # save HTML
-  $html >  $dump
-
-# Sleep a while
-  Start-Sleep -Seconds 30
-} While ($Complete.Count -lt $pcs.Coun
+  $html >  $dump/$file
