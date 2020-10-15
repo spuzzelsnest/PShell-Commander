a6da1832e7795d23d90427ef650e88520584dc6e
Spuzzelsnest <j.mpdesmet@gmail.com>
2020-10-16 01:28:59 +0200

    nmap.ps1 added

---
+++ bin/folderSize.ps1
@@ -0,0 +1,10 @@
+﻿param([string] $dir = "C:\Temp", 
+      [int32] $size = 20000)
+$files = Get-ChildItem $dir
+foreach ($file in $files)
+{
+    if ($file.length -gt $size)
+    {
+        Write-Output $file
+    }
+} 
