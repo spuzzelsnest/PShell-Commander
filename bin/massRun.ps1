eaf50e0746bd8fa8c00444d862e6f11c3c0b8703
Spuzzelsnest <j.mpdesmet@gmail.com>
2020-11-05 20:30:19 +0100

    update massrun

---
+++ bin/massRun.ps1
@@ -0,0 +1,10 @@
+﻿$logs = 'C:\PShell-Commander\bin\Logs'
+$dumpFiles = 'C:\PShell-Commander\bin\_dumpFiles'
+
+$parameters = @{
+  ComputerName = (Get-Content -Path  $logs\TEST-list.txt)
+  InDisconnectedSession = $true
+  FilePath = $dumpFiles+"\tryChoco.ps1"
+  SessionOption = @{OutputBufferingMode="Drop";IdleTimeout=632000000}
+}
+Invoke-Command @parameters
