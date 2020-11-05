967a69a7ce29ab63f43a93499d1620c6e29dce2c
Spuzzelsnest <j.mpdesmet@gmail.com>
2020-11-05 19:40:16 +0100

    cleanup old unfinished scripts

--- bin/tryChoco.ps1
+++ bin/tryChoco.ps1
@@ -1,15 +1,12 @@
 ﻿try{
 
-$command = 'choco -v'
-
-Write-host "Attempting to run: [ $command ]"`n
-
-Invoke-Expression -Command $command 
+    $command = 'choco -v'
+    Write-host "Attempting to run: [ $command ]"`n
+    Invoke-Expression -Command $command 
 
 }
 
 Catch {
-
 
     if($Error[0].Exception.Message.Contains("The term 'choco' is not recognized as the name of a cmdlet")){
     
@@ -17,11 +14,9 @@ Catch {
         Write-host "Lets install" -foreground Green
 
         Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
     
     }else {
     
         Write-Host "Choco has version " $_.Exception.Message -foreground green `n
-    
     }
 }
