79670a93bf7fcb95e1a09b8b127710fb5bdf599f
Jan De Smet <jan.desmet@sint-lievens-houtem.be>
2023-04-16 19:56:58 +0200

    fixed recreatex install

--- bin/_dumpFiles/citrix-Recreatex.ps1
+++ bin/_dumpFiles/citrix-Recreatex.ps1
@@ -12,21 +12,17 @@
 #       VERSION HISTORY:
 #       1.0     10.19.2021 	- Initial release
 #--------------------------------------------------------------------------------
-# Set execution Policy
-#Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
-set-executionpolicy -executionpolicy bypass
 
+Write-host "Citrix Install - Recreatex"
 
-#test if script is running
-$wshell = New-Object -ComObject Wscript.Shell
-$clk = $wshell.Popup("Recreatex install ",0,"Installing Recreatex",0x1)
+$ErrorActionPreference = "silentlycontinue"
 
 # check if registry key exists > base for install
 
-$check = (Get-ItemProperty "HKCU:\SOFTWARE\Citrix\Receiver\InstallDetect\*" | Where { $_.DisplayName -like "Citrix Workspace*" }) -ne $null
+$check = (Get-ItemProperty "HKCU:\SOFTWARE\Citrix\Receiver\InstallDetect\*" | Where { $_.DisplayName -like "Citrix Workspace*" }) -ne $null -ErrorAction SilentlyContinue
 
 If( $check -ne $true) {
 	Write-Host "$software NOT is installed." -ForegroundColor Magenta
     
-    choco install citrix-workspace -y
+    choco install citrix-workspace --force -y
  }
