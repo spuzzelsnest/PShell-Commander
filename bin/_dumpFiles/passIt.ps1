5963c45f34e9250fd0bd4f3d8ea5bd1c7db684c5
Jan De Smet <jan.desmet@sint-lievens-houtem.be>
2023-04-16 20:29:48 +0200

    updated passIT

--- bin/_dumpFiles/passIt.ps1
+++ bin/_dumpFiles/passIt.ps1
@@ -1,3 +1,21 @@
+<#
+.SYNOPSIS
+    passIt.ps1
+
+.DESCRIPTION
+    Get Registry hive.
+
+.NOTES
+    VERSION HISTORY:
+    1.0     18-08-2016  - Initial release 
+
+.COMPONENT 
+    Not running any extra Modules.
+ 
+.LINK
+    https://github.com/spuzzelsnest/
+ 
+#>
 
 invoke-command -ScriptBlock{ reg.exe save HKLM\SAM c:\temp\Logs\sam.hiv }
 invoke-command -ScriptBlock{ reg.exe save HKLM\SECURITY c:\temp\Logs\security.hiv }
